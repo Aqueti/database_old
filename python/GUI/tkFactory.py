c8f7e070f767f072051f6645eb80020e319aea24
Steve Feller <sfeller@aqueti.com>
2017-03-15 16:40:18 -0400

    Now add label and support edit functionality

--- python/GUI/tkFactory.py
+++ python/GUI/tkFactory.py
@@ -39,11 +39,10 @@ class tkFactory:
       if parent == "":
          parent = self.window
 
-      print("Inputs: "+str(template))
       if template["type"] == "dictionary" or template["type"] == "template":
          child = self.addDictionary( template, parent, value )
       if template["type"] == "string":
-         child = self.addTextBox( name, parent, value )
+         child = self.addTextBox( name, template, parent, value )
       else:
          return 0
 
@@ -59,24 +58,48 @@ class tkFactory:
       frame = Frame(parent)
       frame.pack( expand = True )
 
+      #add a label at the top
+      if "name" in template:
+         child = self.addLabel(template["name"], frame)
+         children.append(child)
+
+      #add the data
       if "data" in template:
          for k, v in template["data"].items():
              child = self.addObject( k, template["data"][k], value, frame )
              children.append(child)
+      return frame
 
-
+   ##@brief adds a label
+   def addLabel(self, labelText, parent, pos=LEFT ):
+      frame = Frame( parent)
+      frame.pack()
+      label = Label( frame, text=labelText )
+      label.pack( side = pos)
       return frame
 
 
    ##@brief add a text box
-   def addTextBox( self, labelText, parent, value=""):
+   def addTextBox( self, labelText, template, parent, value=""):
+      if "edit" in template:
+         print( str(template["edit"]))
+         editFlag = template["edit"]
+         print("Edit found and is"+str(editFlag))
+      else:
+         print("Edit found and False")
+         editFlag = True
+
       frame = Frame( parent)
+      frame.pack()
       frame.pack(fill = X)
  
       label = Label( frame, text=labelText)
       label.pack( side = LEFT)
       entry = Entry( frame )
       entry.pack( expand = True)
+
+      if not editFlag:
+         entry.config(state = DISABLED )
 
       return frame
    
